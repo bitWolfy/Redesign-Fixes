360e71c8c18919f7b673aaba478cf65ae9fb5c39
bitWolfy <bondarchuk.kirill@gmail.com>
2020-03-11 19:16:50 -0700

    Wrong script version

--- script/script.user.js
+++ script/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         e621 Redesign Scripts
 // @namespace    bitwolfy.com
-// @version      1.0.0
+// @version      2.0.4
 // @description  Scripting portion of the e621 Redesign Fixes project
 // @author       bitWolfy
 // @homepage     https://github.com/bitWolfy/e621-Redesign-Fixes
