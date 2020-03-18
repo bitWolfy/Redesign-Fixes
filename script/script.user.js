5c561b2ff39c3398ec2edd19a201d9eaf6550e1c
bitWolfy <bondarchuk.kirill@gmail.com>
2020-03-18 00:08:19 -0700

    Version 2.0.8

--- script/script.user.js
+++ script/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         e621 Redesign Scripts
 // @namespace    bitwolfy.com
-// @version      2.0.7
+// @version      2.0.8
 // @description  Scripting portion of the e621 Redesign Fixes project
 // @author       bitWolfy
 // @homepage     https://github.com/bitWolfy/e621-Redesign-Fixes
