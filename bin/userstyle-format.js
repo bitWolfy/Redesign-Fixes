b3aa27d1215e8ca37ddeb4f2ccbcb6f6b6d9cba7
bitWolfy <bondarchuk.kirill@gmail.com>
2020-03-20 01:13:13 -0700

    Re-structure the project for the automated build process

---
+++ bin/userstyle-format.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+const template = fs.readFileSync("./bin/userstyle-template.txt").toString();
+const json = JSON.parse(fs.readFileSync("./package.json"));
+
+var templateReplaced = template.replace(/%NAME%/g, json.displayName)
+    .replace(/%NAMESPACE%/g, json.namespace)
+    .replace(/%DESCRIPTION%/g, json.description)
+    .replace(/%AUTHOR%/g, json.author);
+
+templateReplaced = templateReplaced.replace(/[\u200B-\u200D\uFEFF]/g, '');
+
+if (process.argv[2] === undefined) {
+    templateReplaced = templateReplaced.replace(/%VERSION%/g, json.version);
+} else {
+    templateReplaced = templateReplaced.replace(/%VERSION%/g,
+        json.version.substring(0, json.version.lastIndexOf(".")) +
+        process.argv[2].substring(process.argv[2].lastIndexOf("."))
+    );
+}
+
+fs.writeFileSync("./build/style.min.css", templateReplaced + fs.readFileSync("./build/style.css"));
+fs.writeFileSync("./build/style.user.css",
+    templateReplaced +
+    `@-moz-document domain("e621.net"), domain("e926.net") { \n` +
+    fs.readFileSync("./build/style.css") +
+    ` } \n`
+);
